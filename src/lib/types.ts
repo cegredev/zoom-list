fac8319cdc4703a947d97e3da9769fdb0703a14f
cegredev <cedricg@gmx.net>
2023-04-15 21:12:42 +0200

    Installed Luxon, setup types

---
+++ src/lib/types.ts
@@ -0,0 +1,14 @@
+import type { DateTime, Duration } from 'luxon';
+
+export type ID = number;
+
+export interface Client {
+	id: ID;
+	name: string;
+}
+
+export interface ZoomEntry {
+	client: ID;
+	start: DateTime;
+	duration: Duration;
+}
