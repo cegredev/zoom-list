51590c14ebbdbe895ef91d826eec07fa03a4d7fc
cegredev <cedricg@gmx.net>
2023-04-16 12:49:51 +0200

    Setup UI

---
+++ src/lib/uiconsts.ts
@@ -0,0 +1,14 @@
+export const fullMonths = [
+	'Januar',
+	'Februar',
+	'März',
+	'April',
+	'Mai',
+	'Juni',
+	'Juli',
+	'August',
+	'September',
+	'Oktober',
+	'November',
+	'Dezember'
+];
--- src/routes/entry/+page.svelte
+++ src/routes/entry/+page.svelte
@@ -1,45 +1,74 @@
 <script lang="ts">
 	import { DateTime } from 'luxon';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
+	import { fullMonths } from '$lib/uiconsts';
 
 	const DATE_DISPLAY_FORMAT = 'dd.MM.yyyy';
 
 	let date = DateTime.now();
 	$: monthStart = date.startOf('month');
 	$: totalDays = date.endOf('month').day;
+	$: year = date.year;
+	$: month = date.month;
 </script>
 
 <div class="wrapper">
-	{#each { length: totalDays } as _, days}
-		<div>
-			<div>{monthStart.plus({ days }).toFormat(DATE_DISPLAY_FORMAT)}</div>
-			{#if Math.random() < 0.5}
-				<div>Noch keine Daten vorhanden</div>
-			{:else}
-				<button>Löschen</button>
-			{/if}
-		</div>
-	{/each}
-	<div class="bottom-bar">
-		<div>Jahr</div>
-		<div>Monat</div>
-		<button>Hinzufügen</button>
+	<div class="entries">
+		{#each { length: totalDays } as _, days}
+			<div class="row">
+				<div class="badge badge-lg text-primary-content">
+					{monthStart.plus({ days }).toFormat(DATE_DISPLAY_FORMAT)}
+				</div>
+				{#if Math.random() < 0.5}
+					<div>Noch keine Daten vorhanden</div>
+				{:else}
+					<div>Einträge: 59</div>
+					<button class="btn btn-sm btn-outline btn-secondary">Löschen</button>
+				{/if}
+			</div>
+		{/each}
+	</div>
+	<div class="bottom-bar row">
+		<input type="number" name="year" bind:value={year} class="input input-bordered w-32" />
+
+		<select name="month" class="select" bind:value={month}>
+			{#each fullMonths as month, i}
+				<option value={i + 1}>{month}</option>
+			{/each}
+		</select>
+
+		<div class="divider divider-horizontal" />
+
+		<button class="btn btn-primary">Hinzufügen</button>
 	</div>
 </div>
 
 <style>
-	div.wrapper > div {
+	.wrapper {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.entries {
+		flex: auto;
+		overflow-y: auto;
+	}
+
+	.row {
 		display: flex;
 		flex-direction: row;
+		gap: 10px;
+		align-items: center;
+
+		padding: 15px;
+		border-bottom: 1px solid hsl(var(--b2));
 	}
 
 	.bottom-bar {
-		background-color: white;
-		position: fixed;
-		bottom: 0;
-		width: 100%;
+		@apply bg-base-200;
+
+		flex: 0;
 		padding: 5px;
+		padding-left: 15px;
 	}
 </style>
