c46af4503847f74ae7318ad13e6875fc8fd9833f
cegredev <cedricg@gmx.net>
2023-04-16 14:11:27 +0200

    Creating database on first open

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,90 +3,80 @@
     windows_subsystem = "windows"
 )]
 
+use rusqlite::{Connection, Result};
 use std::{fs, path::PathBuf};
 
-use rusqlite::{Connection, Result};
-use tauri::PathResolver;
-
 #[derive(Debug)]
-struct Person {
-    id: i32,
+struct Client {
+    id: u32,
     name: String,
-    data: Option<Vec<u8>>,
 }
 
 const DATABASE_FOLDER_NAME: &'static str = "databases";
+const DATABASE_FILE_NAME: &'static str = "db.db3";
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let mut path = app
+            let mut dir_path = app
                 .path_resolver()
                 .app_data_dir()
                 .ok_or("AppDataDir not found")?;
-            path.push(DATABASE_FOLDER_NAME);
+            dir_path.push(DATABASE_FOLDER_NAME);
 
-            if !path.exists() {
-                fs::create_dir_all(path.clone())?;
+            let mut db_path = dir_path.clone();
+            db_path.push(DATABASE_FILE_NAME);
+
+            if !db_path.exists() {
+                fs::create_dir_all(dir_path.clone())?;
+
+                init_db(db_path)?;
             }
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![init_db])
+        // .invoke_handler(tauri::generate_handler![init_db])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
-#[tauri::command]
-fn init_db(app_handle: tauri::AppHandle) {
-    let mut path = app_handle.path_resolver().app_data_dir().unwrap();
-    path.push(DATABASE_FOLDER_NAME);
-    path.push("db.db3");
+// #[tauri::command]
+// fn init_db(app_handle: tauri::AppHandle) {
+//     let mut path = app_handle.path_resolver().app_data_dir().unwrap();
+//     path.push(DATABASE_FOLDER_NAME);
+//     path.push("db.db3");
 
-    println!("path: {:?}", path);
+//     println!("path: {:?}", path);
 
-    init_db_logic(path).expect("Init failed");
-}
+//     init_db_logic(path).expect("Init failed");
+// }
 
-fn init_db_logic(path: PathBuf) -> Result<()> {
-    println!("initing");
+fn init_db(path: PathBuf) -> Result<()> {
+    println!("Creating database:");
 
     let conn = Connection::open(path)?;
 
+    println!("Creating clients table...");
     conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
-            data  BLOB
+        "CREATE TABLE clients (
+            id    INTEGER UNSIGNED PRIMARY KEY,
+            name  TEXT NOT NULL
+            )",
+        (), // empty list of pa rameters.
+    )?;
+    println!("Success!");
+
+    println!("Creating records table...");
+    conn.execute(
+        "CREATE TABLE records (
+            client_id   INTEGER UNSIGNED,
+            start_ms    INTEGER UNSIGNED,
+            duration_s  INTEGER UNSIGNED,
+            FOREIGN KEY(client_id) REFERENCES clients(id)
         )",
-        (), // empty list of parameters.
+        (),
     )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
-
-    println!("inserted");
-
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-
-    println!("done");
+    println!("Success!");
 
     Ok(())
 }
