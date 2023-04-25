468dfb4bb1339a33e8434eabcfee54fa9ce6a3f6
cegredev <57912512+cegredev@users.noreply.github.com>
2023-04-26 00:11:31 +0200

    Force selection of path

--- src/lib/tauri.ts
+++ src/lib/tauri.ts
@@ -1,14 +1,14 @@
-// import { invoke as invokeTauri } from '@tauri-apps/api/tauri';
-
 let _invoke: any;
 let _open: any;
+let _message: any;
 
 async function fixTauriFunctions() {
 	const { invoke } = await import('@tauri-apps/api');
 	_invoke = invoke;
 
-	const { open } = await import('@tauri-apps/api/dialog');
+	const { open, message } = await import('@tauri-apps/api/dialog');
 	_open = open;
+	_message = message;
 }
 
 // There is a bug with Vite that forces it to use SSR even if disabled
@@ -24,4 +24,8 @@ export async function invoke(cmd: string, options?: any): Promise<any> {
 
 export async function open(options?: any): Promise<string[] | string | null> {
 	return await _open(options);
+}
+
+export async function message(message: string, options?: any): Promise<void> {
+	return await _message(message, options);
 }
--- src/routes/clients/ClientRow.svelte
+++ src/routes/clients/ClientRow.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import DeleteButton from '$lib/components/DeleteButton.svelte';
-	import { invoke } from '$lib/tauri';
+	import { appConfig } from '$lib/stores';
+	import { invoke, message } from '$lib/tauri';
 	import type { Client } from '$lib/types';
 
 	export let year: number, month: number;
 	export let client: Client;
 	export let onDelete: () => Promise<void>;
+
+	let outputMessage = '';
 </script>
 
 <div class="badge badge-lg text-primary-content">
@@ -15,6 +19,14 @@
 <button
 	class="btn btn-sm btn-outline btn-secondary"
 	on:click={async () => {
+		if ($appConfig.path === '') {
+			await message('Bitte ein Output-Verzeichnis wählen!');
+
+			await goto('/settings');
+
+			return;
+		}
+
 		const file = await invoke('generate_report', { clientId: client.id, year, month });
 
 		console.log('File:', file);
@@ -22,3 +34,5 @@
 >
 
 <DeleteButton {onDelete} />
+
+{outputMessage}
